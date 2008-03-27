b4d450b538fc22d48a6ccb23deafda15a721fe1d
davyboyhayes <davyboyhayes@dev.java.net>
2008-03-27 20:10:21 +0000

    A helper class that stores a single instance of the various icons. Using multiple references to the same images should reduce the memory footprint.

---
+++ client/src/org/hudson/trayapp/gui/icons/Icons.java
@@ -0,0 +1,13 @@
+package org.hudson.trayapp.gui.icons;
+
+import java.awt.Toolkit;
+
+import javax.swing.ImageIcon;
+
+public class Icons {
+	public static ImageIcon H0019 = new ImageIcon(Toolkit.getDefaultToolkit().getImage(Icons.class.getResource("/org/hudson/trayapp/gui/icons/16x16/health-00to19.gif")));
+	public static ImageIcon H2039 = new ImageIcon(Toolkit.getDefaultToolkit().getImage(Icons.class.getResource("/org/hudson/trayapp/gui/icons/16x16/health-20to39.gif")));
+	public static ImageIcon H4059 = new ImageIcon(Toolkit.getDefaultToolkit().getImage(Icons.class.getResource("/org/hudson/trayapp/gui/icons/16x16/health-40to59.gif")));
+	public static ImageIcon H6079 = new ImageIcon(Toolkit.getDefaultToolkit().getImage(Icons.class.getResource("/org/hudson/trayapp/gui/icons/16x16/health-60to79.gif")));
+	public static ImageIcon H80PL = new ImageIcon(Toolkit.getDefaultToolkit().getImage(Icons.class.getResource("/org/hudson/trayapp/gui/icons/16x16/health-80plus.gif")));
+}
