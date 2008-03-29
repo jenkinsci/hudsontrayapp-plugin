d7c2e3321ca48081615967a9b465a7c22073b83d
davyboyhayes <davyboyhayes@dev.java.net>
2008-03-29 20:02:52 +0000

    Added this class to abstract the implementation of the TrayIcon and other system services. Under Java 6 we should use that implementation, as it is more robust, and under Java 1.4-Java 5, we should use the JDIC implementation.
    This class is the Java 6 implementation of this service.

---
+++ client/srcjava6/org/hudson/trayapp/gui/tray/AWTTrayIcon.java
@@ -0,0 +1,113 @@
+package org.hudson.trayapp.gui.tray;
+
+import java.awt.AWTException;
+import java.awt.Component;
+import java.awt.Desktop;
+import java.awt.PopupMenu;
+import java.awt.SystemTray;
+import java.awt.TrayIcon;
+import java.awt.TrayIcon.MessageType;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.net.URI;
+
+import javax.swing.ImageIcon;
+import javax.swing.JPopupMenu;
+
+public class AWTTrayIcon extends TrayIconImplementation {
+
+	private TrayIcon trayIcon;
+	private TrayIconMouseListenerWithJPopupMenu mouseListener;
+	
+	public AWTTrayIcon(ImageIcon icon) {
+		super(icon);
+		trayIcon = new TrayIcon(icon.getImage());
+		try {
+			SystemTray.getSystemTray().add(trayIcon);
+		} catch (AWTException e) {
+			e.printStackTrace();
+		}
+		mouseListener = new TrayIconMouseListenerWithJPopupMenu();
+		trayIcon.addMouseListener(mouseListener);
+		System.out.println("AWTTrayIcon");
+	}
+	
+	@Override
+	public void addActionListener(ActionListener listener) {
+		trayIcon.addActionListener(listener);
+	}
+
+	@Override
+	public void displayMessage(String caption, String message, int type) {
+		MessageType messageType = TrayIcon.MessageType.NONE;
+		switch (type) {
+		case TrayIconImplementation.INFO_MESSAGE_TYPE:
+			messageType = TrayIcon.MessageType.INFO;
+			break;
+		case TrayIconImplementation.WARNING_MESSAGE_TYPE:
+			messageType = TrayIcon.MessageType.WARNING;
+			break;
+		case TrayIconImplementation.ERROR_MESSAGE_TYPE:
+			messageType = TrayIcon.MessageType.ERROR;
+			break;
+		}
+		trayIcon.displayMessage(caption, message, messageType);
+	}
+
+	@Override
+	public void setIcon(ImageIcon icon) {
+		trayIcon.setImage(icon.getImage());
+	}
+
+	@Override
+	public void setIconAutoSize(boolean autoSize) {
+		trayIcon.setImageAutoSize(autoSize);
+	}
+
+	@Override
+	public void setPopupMenu(JPopupMenu popupmenu) {
+		mouseListener.popupMenu = popupmenu;
+	}
+
+	@Override
+	public void setToolTip(String tooltip) {
+		trayIcon.setToolTip(tooltip);
+	}
+	
+	public void browse(URI uri) throws Exception {
+		Desktop.getDesktop().browse(uri);
+	}
+	
+	private class TrayIconMouseListenerWithJPopupMenu implements MouseListener{
+		
+		private JPopupMenu popupMenu = null;
+
+		@Override
+		public void mouseClicked(MouseEvent e) { }
+
+		@Override
+		public void mouseEntered(MouseEvent e) { }
+
+		@Override
+		public void mouseExited(MouseEvent e) { }
+
+		@Override
+		public void mousePressed(MouseEvent e) {
+			if (e.isPopupTrigger() && popupMenu != null) {
+				popupMenu.setLocation(e.getX(), e.getY());
+				popupMenu.setInvoker(popupMenu);
+				popupMenu.setVisible(true);
+			}
+		}
+
+		@Override
+		public void mouseReleased(MouseEvent e) {
+			if (e.isPopupTrigger() && popupMenu != null) {
+				popupMenu.setLocation(e.getX(), e.getY());
+				popupMenu.setInvoker(popupMenu);
+				popupMenu.setVisible(true);
+			}
+		}
+	}
+}
