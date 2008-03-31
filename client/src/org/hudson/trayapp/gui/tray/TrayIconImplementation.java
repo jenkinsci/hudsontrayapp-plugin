78d763b38bdb5f60185fc80792d5d06c062cc830
davyboyhayes <davyboyhayes@dev.java.net>
2008-03-31 21:05:28 +0000

    Added a new method that provides a simple place to call a message to be displayed if an exception occurs.

--- client/src/org/hudson/trayapp/gui/tray/TrayIconImplementation.java
+++ client/src/org/hudson/trayapp/gui/tray/TrayIconImplementation.java
@@ -8,11 +8,15 @@ import javax.swing.JPopupMenu;
 
 public abstract class TrayIconImplementation {
 	
+	private static TrayIconImplementation TRAYICON;
+	
 	public final static int ERROR_MESSAGE_TYPE = 1;
 	public final static int WARNING_MESSAGE_TYPE = 2;
 	public final static int INFO_MESSAGE_TYPE = 3;
 
-	public TrayIconImplementation(ImageIcon img) {}
+	public TrayIconImplementation(ImageIcon img) {
+		TRAYICON = this;
+	}
 	
 	public abstract void setIconAutoSize(boolean autoSize);
 	
@@ -27,4 +31,8 @@ public abstract class TrayIconImplementation {
 	public abstract void setIcon(ImageIcon icon);
 	
 	public abstract void browse(final URI uri) throws Exception;
+	
+	public static void displayException(String title, String message, Exception e) {
+		TRAYICON.displayMessage(title, message + "\nException:" + e.toString(), ERROR_MESSAGE_TYPE);
+	}
 }
